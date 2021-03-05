4416914d8eaa17c30d357fa73f5e50551503fd48
raaedk <raaedk@gmail.com>
2021-03-05 16:53:41 -0500

    basic correlation implemented

--- main.py
+++ main.py
@@ -5,6 +5,8 @@ Created on Fri Mar  5 16:25:14 2021
 @author: raaed
 """
 
+#evolve this to show correlations for all pairs
+
 import pandas_datareader.data as pdr
 import numpy as np
 import pandas as pd
@@ -32,9 +34,16 @@ ENDPOINT = 'https://ftx.com/api'
 r1 = requests.get(ENDPOINT + '/markets/{market_name}/candles?resolution={resolution}&limit={limit}'.format(market_name = _market1Name, resolution=_resolution, limit=_limit))
 r2 = requests.get(ENDPOINT + '/markets/{market_name}/candles?resolution={resolution}&limit={limit}'.format(market_name = _market2Name, resolution=_resolution, limit=_limit))
 
-print(r1.json()['result'])
+#print(r1.json()['result'])
 
 df1 = pd.DataFrame(r1.json()['result'])
 df2 = pd.DataFrame(r2.json()['result'])
 
-print(df2)
+#print(df2)
+
+s1 = df1['close']
+s2 = df2['close']
+
+print("The correlation between BTC-ETH is {corr}".format(corr = s1.corr(s2)))
+
+#print(df1.corr())
